82ed380d48a79af75407814f6a9457057cf399b7
sreejithmm <sreejith.mm@gmail.com>
2020-05-10 13:16:50 +0530

    exercise2

--- sreejith/script.js
+++ sreejith/script.js
@@ -1,28 +1,47 @@
-document.write("Hello world--");
-var req = new XMLHttpRequest();
-req.onload = reqListener;
-req.onerror = reqError;
-req.open('get', 'https://jsonplaceholder.typicode.com/todos/1', true);
-req.send(); 
-req.onreadystatechange = reqParser;
-console.log("Request sent");
+function loadJSONDoc() {
+    var req = new XMLHttpRequest();
+    req.onerror = reqError;
+    req.onload = reqParser;
 
+    req.open('get', 'https://jsonplaceholder.typicode.com/todos/1', true);
+    req.send(); 
 
-function reqListener() {
-    var data = JSON.parse(this.responseText);
-    console.log(data);
-  }
+    console.log("Request sent");
+}
+
   
 function reqError(err) {
     console.log('Fetch Error :-S', err);
 }
 
 function reqParser() {
-  
-  if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("demo").innerHTML =
-    this.responseText;
+
+  var table = document.getElementById("myTable");
+  var jsonobj = JSON.parse(this.responseText);
+  var length = Object.keys(jsonobj).length;
+  var tablelength = table.rows.length;
+  var i;
+
+  console.log("Table legnth :-S",tablelength)
+
+  for (i = 0; i < tablelength; i++) {
+    table.deleteRow(0);
   }
+
+  console.log("JSON Key Length:-S",length);
+  var row = table.insertRow(0);
+
+  for (i = 0; i < length; i++) {
+    row.insertCell(i).innerHTML = Object.keys(jsonobj)[i];
+  }
+  var row1 = table.insertRow(1);
+
+  for(i=0; i< length; i++) {
+    row1.insertCell(i).innerHTML =  Object.values(jsonobj)[i];
+  }
+  
+  console.log("Finished")
+  
 
 }
   
